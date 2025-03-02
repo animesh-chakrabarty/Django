eb78d66937a520126e00f2920b553587f4e0507f
animesh-chakrabarty <animeshchakrabarty.dev@gmail.com>
2025-03-02 21:00:26 +0530

    added login required in post creation page

--- posts/urls.py
+++ posts/urls.py
@@ -5,5 +5,6 @@ app_name = 'posts'
 
 urlpatterns = [
     path('', views.posts_list, name="feed"),
+    path('create/', views.create_post, name="create-post"),
     path('<slug:slug>', views.post, name="post" )
 ]
